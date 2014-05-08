e0d3645b9ebfeddf71eca12661e9980047292f1c
OPolpo <git-projects@omnip.it>
2014-05-08 23:01:54 +0200

    added a table and other stuff

--- ass_panda_pt1.cxx
+++ ass_panda_pt1.cxx
@@ -9,17 +9,7 @@ PandaFramework framework;
 PT(AsyncTaskManager) taskMgr = AsyncTaskManager::get_global_ptr();
 PT(ClockObject) globalClock = ClockObject::get_global_clock();
 NodePath camera;
-NodePath myCube;
-
-// Task to move the Cube
-AsyncTask::DoneStatus rotatingCube(GenericAsyncTask* task, void* data) {
-	double time = globalClock->get_real_time();
-	double angledegrees = time * 40.0;
-	myCube.set_hpr(angledegrees, angledegrees/40*20, 0);
-		
-	return AsyncTask::DS_cont;
-}
-
+NodePath room, table, mantis;
 
 void moveForward(const Event * theEvent, void * data){
 	cout << "forward" <<endl;
@@ -41,6 +31,41 @@ void Exit(const Event * theEvent, void * data){
 	exit(0);
 }
 
+void modelSetup(WindowFramework *window){
+
+
+	room = window->load_model(framework.get_models(),"models/endroom/EndRoom");
+	room.reparent_to(window->get_render());
+	room.set_scale(0.1);
+	room.set_pos(0, 0, 0);
+	room.set_hpr(0, 0, 0);
+
+	table = window->load_model(framework.get_models(),"models/dining_table_2/DiningTable2");
+	table.reparent_to(window->get_render());
+	table.set_scale(1);
+	table.set_pos(0, 0, 0);
+	table.set_hpr(0, 0, 0);
+
+	mantis = window->load_model(framework.get_models(),"models/mantis/mantis");
+	mantis.reparent_to(table);
+	mantis.set_scale(2);
+	mantis.set_pos(1.5, 0, 2);
+	mantis.set_hpr(170, 0, 0);
+
+
+	
+}
+
+void inputBinding(){
+	framework.define_key("escape", "exit", Exit, 0);
+	framework.define_key("arrow_up", "camera forward", moveForward, 0);
+	framework.define_key("arrow_down", "camera backward", moveBackward, 0);
+	framework.define_key("arrow_left", "camera left", moveLeft, 0);
+	framework.define_key("arrow_right", "camera right", moveRight, 0);
+}
+
+
+
 int main(int argc, char *argv[]) {
 	// Open a new window framework and set the title
 	framework.open_framework(argc, argv);
@@ -50,27 +75,13 @@ int main(int argc, char *argv[]) {
 
 	//keyboard Management
 	window->enable_keyboard();
-	framework.define_key("escape", "exit", Exit, 0);
-	framework.define_key("arrow_up", "camera forward", moveForward, 0);
-	framework.define_key("arrow_down", "camera backward", moveBackward, 0);
-	framework.define_key("arrow_left", "camera left", moveLeft, 0);
-	framework.define_key("arrow_right", "camera right", moveRight, 0);
-
+	
+	inputBinding();
+	modelSetup(window);
 
 	camera = window->get_camera_group(); // Get the camera and store it
-	camera.set_pos(0,0,0);
-	camera.set_hpr(0, 0, 0);
-	
-	// Load cube
-	//myCube = window->load_model(framework.get_models(),"models/misc/rgbCube");
-	myCube = window->load_model(framework.get_models(),"models/box");
-	myCube.reparent_to(window->get_render());
-	myCube.set_scale(0.5);
-	myCube.set_pos(0, 5, 0);
-	myCube.set_hpr(45,45,0);
-	
-	// Add our task.
-	taskMgr->add(new GenericAsyncTask("Spins the camera", &rotatingCube, (void*) NULL));
+	camera.set_pos(0,-20,5);
+	camera.set_hpr(0, -10, 0);
 	
 	// Run the engine.
 	framework.main_loop();
